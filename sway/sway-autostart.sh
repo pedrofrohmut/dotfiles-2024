2bce105ac3560d89b82c6c006d4c1d6972117dba
pedrofrohmut <pedrofrohmut@gmail.com>
2026-09-01 10:58:28 -0300

    Sway: layout keybinds; Fix dbus to open file explorer on apps; Notifications with mako; minor stuff in the waybar;

--- sway/sway-autostart.sh
+++ sway/sway-autostart.sh
@@ -20,11 +20,11 @@ blueman-applet &
 # PolicyKit Authentication Agent (PolicyKit Authentication Agent)
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
+# Notification Daemon
+mako &
+
 # Color temperature (default: T 6500 t 4500)
 wlsunset -T 5700 -t 3500 -g 1.0 -S 06:00 -s 18:00 &
 
 # Setup the background
 swaybg -i $HOME/media/wallpaper/1332407.png -m stretch &
-
-# Lets sway layout be dynamic
-autotiling &
